a34be480343edeb18432c220c737bf82bd02f03d
Adelodun Abraham <ab.adelodun@gmail.com>
2024-05-19 00:49:39 +0100

    Cleared screen after every guess

--- hangman.py
+++ hangman.py
@@ -1,68 +1,21 @@
 import random as rn
 import pyfiglet
+import os
+
 word_list = [
-    "abyss",
-    "askew",
-    "awkward",
-    "avenue",
-    "banjo",
-    "bayou",
-    "blitz",
-    "bookworm",
-    "boxcar",
-    "buckaroo",
-    "buffalo",
-    "buzzing",
-    "cobweb",
-    "crypt",
-    "cycle",
-    "faking",
-    "fixable",
-    "glowworm",
-    "gossip",
-    "haiku",
-    "haphazard",
-    "icebox",
-    "jazzy",
-    "jigsaw",
-    "joking",
-    "lucky",
-    "matrix",
-    "numbskull",
-    "onyx",
-    "pajama",
-    "pixel",
-    "puppy",
-    "quartz",
-    "queue",
-    "quorum",
-    "rhythm",
-    "scratch",
-    "snazzy",
-    "staff",
-    "stretch",
-    "subway",
-    "swivel",
-    "topaz",
-    "unzip",
-    "uptown",
-    "walkway",
-    "waltz",
-    "wave",
-    "waxy",
-    "wimpy",
-    "woozy",
-    "yippee",
-    "zigzag",
-    "zombie"
+    "abyss", "askew", "awkward", "avenue", "banjo", "bayou", "blitz", "bookworm",
+    "boxcar", "buckaroo", "buffalo", "buzzing", "cobweb", "crypt", "cycle",
+    "faking", "fixable", "glowworm", "gossip", "haiku", "haphazard", "icebox",
+    "jazzy", "jigsaw", "joking", "lucky", "matrix", "numbskull", "onyx", "pajama",
+    "pixel", "puppy", "quartz", "queue", "quorum", "rhythm", "scratch", "snazzy",
+    "staff", "stretch", "subway", "swivel", "topaz", "unzip", "uptown", "walkway",
+    "waltz", "wave", "waxy", "wimpy", "woozy", "yippee", "zigzag", "zombie"
 ]
 
-
-
-chosen_word=rn.choice(word_list)
+chosen_word = rn.choice(word_list)
 ascii_art = pyfiglet.figlet_format("hangman", font='big')
-stages=[
-"""
+stages = [
+    """
     +---+
     |   |
     O   |
@@ -118,36 +71,46 @@ stages=[
         |
         |
 ===========
-"""]
+"""
+]
 
-display=[]
-for letters in chosen_word:
-    display+="_"
-lives=5
+display = ["_" for _ in chosen_word]
+lives = 5
 print(ascii_art)
-print(chosen_word)
-
 
 while True:
-    guess=input("Guess a letter: ")
-    if guess in display:
-        print(f"You've already guessed {guess}")
-    guess=guess.lower()
-    for position in range(len(chosen_word)):
-        letter=chosen_word[position]
-        if letter == guess:
-            display[position] = letter
-    print(f"{"_".join(display)}")
+    print(f"{' '.join(display)}")
+    guess = input("Guess a letter: ").lower()
     
-    if guess not in chosen_word:
+    if guess in display:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(ascii_art)
+        print(stages[lives])
+        print(f"You've already guessed {guess}")
+        continue
+    
+    if guess in chosen_word:
+        for position in range(len(chosen_word)):
+            if chosen_word[position] == guess:
+                display[position] = guess
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(ascii_art)
+        print(stages[lives])
+    else:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(ascii_art)
+        print(stages[lives])
+        lives -= 1
         print(f"You guessed {guess}, that's not in the word. You lose a life!")
-        lives-=1
-        if lives==0:
-            print(stages[0])
-            print("Oh no! The hangman has met his fate. Better luck next time!")
-            break
+    
     
     if "_" not in display:
+        print(f"{' '.join(display)}")
         print("Hooray! You saved the hangman!")
         break
-    print(stages[lives])
+    
+    if lives == 0:
+        print(stages[0])
+        print(f"The word was '{chosen_word}'.")
+        print("Oh no! The hangman has met his fate. Better luck next time!")
+        break
